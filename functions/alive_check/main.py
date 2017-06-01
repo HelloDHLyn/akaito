4ef4a9482985c5b66a3ef3f276d279e20265a0cd
Do Hoerin <hellodhlyn@gmail.com>
2017-06-01 21:46:16 +0900

    Added 'alive_check' lambda function

--- functions/alive_check/main.py
+++ functions/alive_check/main.py
@@ -1,2 +1,29 @@
+import os
+
+from botocore.vendored.requests.packages import urllib3
+
+
+def send_message(url):
+    chatbot_url = os.environ['CHATBOT_URL'] + '/sendMessage'
+    fields = {
+        'chat_id': os.environ['CHAT_ID'],
+        'text': f"[Akaito] Server is down!\n{url}",
+        'parse_mode': 'HTML'
+    }
+
+    http = urllib3.PoolManager()
+    r = http.request('POST', chatbot_url, fields=fields)
+
+    if r.status >= 400:
+        exit(f"Failed to send message via telegram... ({r.status} {r.reason})")
+
+
 def handle(event, context):
-    print("Hello, world!")
+    url = event['Records'][0]['Sns']['MessageAttributes']['TargetUrl']['Value']
+    if not url:
+        exit(f"Failed to check connection... No url provided.")
+
+    http = urllib3.PoolManager()
+    r = http.request('GET', url)
+    if r.status >= 300:
+        send_message(url)
